a853ff3c9e7ea4dbced7e7fb9f3ae967bf3ff29d
Avusar <avusargaming@gmail.com>
2022-11-30 00:47:13 +0300

    StartDelegatingTasks

---
+++ Selenium/Exceptions/APIException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Exceptions
+{
+    public class APIException : Exception
+    {
+        public APIException(string json) : base(json)
+        {
+
+        }
+    }
+}
---
+++ Selenium/Providers/TasksProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Providers
+{
+    public class TasksProvider : ITasksProvider
+    {
+        public Task DelegateTask<T>(Func<T, Task> task, IEnumerable<T> list)
+        {
+            List<Task> tasks = new List<Task>();
+
+            foreach (var obj in list)
+            {
+                tasks.Add(task(obj));
+            }
+
+            return Task.WhenAll(tasks.ToArray());
+        }
+    }
+
+    public interface ITasksProvider
+    {
+        public Task DelegateTask<T>(Func<T, Task> task, IEnumerable<T> list);
+    }
+}
